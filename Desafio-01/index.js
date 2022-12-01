54a55e0117646f5a18ed4ecd03cc8e4c313d2a18
Jessica Viana <jeh.fonseca93@gmail.com>
2022-12-01 13:15:24 -0300

    Commit

---
+++ Desafio-01/index.js
@@ -0,0 +1,15 @@
+var a = window.document.getElementById('b1')
+var b = window.document.getElementById('b2')
+var c = window.document.getElementById('b3')
+
+function clicar1() {
+    alert('Você clicou no 1º botão!')
+}
+
+function clicar2() {
+    alert('Você clicou no 2º botão!')
+}
+
+function clicar3() {
+    alert('Você clicou no 3º botão!')
+}
